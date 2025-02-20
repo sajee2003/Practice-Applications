bb26be8b6c7764b1e9a089c3d5d814b066e83a37
sajee2003 <kanesarajasajeevan295@gmail.com>
2025-02-20 21:34:47 +0530

    Initial commit on CreatingEmailUsing.Net

---
+++ WebApplication1/Controllers/EmailController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+using System;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+  
+        
+            private readonly EmailService _emailService;
+
+        //ok 
+
+            public EmailController(EmailService emailService)
+            {
+                _emailService = emailService;
+            }
+
+            [HttpPost("send")]
+            public IActionResult SendEmail([FromBody] EmailRequest request)
+            {
+                try
+                {
+                // Generate Email Content
+                var emailBody = _emailService.GenerateEmailContent(request.RecipientName, request.body);
+
+                    // Send Email
+                    _emailService.SendEmail(request.RecipientName, request.RecipientEmail, "Welcome to Our Service!", emailBody, request.body);
+
+                    return Ok(new { message = "Email sent successfully!" });
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new { error = ex.Message });
+                }
+            }
+        }
+
+        public class EmailRequest
+        {
+            public string RecipientName { get; set; }
+            public string RecipientEmail { get; set; }
+
+              public string body { get; set; }
+    }
+
+    }
---
+++ WebApplication1/EmailService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Configuration;
+using MimeKit;
+using System.Net;
+using System.Net.Mail;
+
+using MailKit.Net.Smtp;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+
+
+namespace WebApplication1
+{
+    //public interface IEmailService
+    //{
+    //    Task SendEmail(string toEmail, string subject, string body);
+    //}
+
+    public class EmailService
+    {
+
+        public void SendEmail(string recipientName, string recipientEmail, string subject, string body, string leavebody)
+        {
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress("Cloudy (pvt) ltd", "kanesarajasajeevan295@gmail.com"));
+            email.To.Add(new MailboxAddress(recipientName, recipientEmail));
+            email.Subject = subject;
+
+            var bodyBuilder = new BodyBuilder { TextBody = body };
+            email.Body = bodyBuilder.ToMessageBody();
+
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                smtp.Authenticate("kanesarajasajeevan295@gmail.com", "yhwn mnib vdra xeex");
+                smtp.Send(email);
+                smtp.Disconnect(true);
+            }
+        }
+
+        public string GenerateEmailContent(string recipientName, string ebody)
+        {
+            return $@"
+                  
+
+                 hi {recipientName}
+       
+                    {ebody}
+                    ";
+        }
+
+    }
+}
