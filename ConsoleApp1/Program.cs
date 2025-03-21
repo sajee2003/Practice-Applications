bf8dd321219d38eca2d5bd26570bbe0a8a8e75a0
sajee2003 <kanesarajasajeevan295@gmail.com>
2025-03-22 00:10:20 +0530

    Your commit message here

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //----------- Square Star Pattern  --------------------
+
+            //-----------------------------------
+            //*****
+            //*****
+            //*****
+            //*****
+            //*****
+
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < 5; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+
+            // Right Triangle Star Pattern-------------------
+
+            //*
+            //**
+            //***
+            //****
+            //*****
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < i+1; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+
+
+            //Inverted Half-Pyramid of *
+
+            // *****
+            //****
+            //***
+            //**
+            //*
+
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for ( int j = 5; j > i; j--)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+     
